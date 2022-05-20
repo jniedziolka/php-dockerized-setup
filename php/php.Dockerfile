d4df7f7f748d6bbd9aec1dd161d630a77a8cf9e0
jniedziolka <jakub.niedziolka@o2.pl>
2022-05-21 00:48:56 +0200

    Added database service to compose file, modified PHP Dockerfile to install pdo_mysql extension and modified test.php file to test actual connection

--- php/php.Dockerfile
+++ php/php.Dockerfile
@@ -3,4 +3,6 @@ FROM php:8.1-fpm
 RUN curl -sS https://getcomposer.org/installer |php \
     && mv composer.phar /usr/local/bin/composer
 
+RUN docker-php-ext-install pdo_mysql
+
 WORKDIR /var/www/html
--- test.php
+++ test.php
@@ -1,3 +1,14 @@
 <?php
 
-echo "Hello World from PHP";
+$serverName = 'db';
+$databaseName = 'db';
+$username = 'user';
+$password = 'password';
+
+try {
+    $conn = new PDO("mysql:host=$serverName;dbname=$databaseName", $username, $password);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "Connected successfully";
+} catch(PDOException $e) {
+    echo "Connection failed: " . $e->getMessage();
+}
